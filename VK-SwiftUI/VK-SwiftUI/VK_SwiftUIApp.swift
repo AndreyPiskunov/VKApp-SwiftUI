f594b6644e07c720e677f8788a18945af80c55d2
Andrey91 <mistaandrey@icloud.com>
2022-12-23 13:52:04 +0500

    Initiate project

---
+++ VK-SwiftUI/VK-SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK-SwiftUI
+//
+//  Created by Andrey Piskunov on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
