2a7f48e212604f055c4a92fe1be73a730b9120a9
Andrey91 <mistaandrey@icloud.com>
2022-12-25 18:13:41 +0500

    Add login screen UI

---
+++ VK-SwiftUI/VK-SwiftUI/Extensions/ExtensionsUI.swift
@@ -0,0 +1,14 @@
+//
+//  ExtensionsView.swift
+//  VK-SwiftUI
+//
+//  Created by Andrey Piskunov on 25.12.2022.
+//
+
+import SwiftUI
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
---
+++ VK-SwiftUI/VK-SwiftUI/LoginScreen/LoginScreenView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  VK-SwiftUI
+//
+//  Created by Andrey Piskunov on 23.12.2022.
+//
+
+import SwiftUI
+import Combine
+
+struct LoginScreenView: View {
+    @State private var login = ""
+    @State private var password = ""
+    @State private var shouldShowLogo: Bool = true
+    
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false }
+    ).removeDuplicates()
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack(spacing: 100) {
+                    if shouldShowLogo {
+                        //Logo image
+                        Image("VK")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .padding(.top, 100)
+                    }
+                    VStack {
+                        //Login TextField
+                        TextField("Введите логин", text: $login)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(12)
+                            .padding(8)
+                            .padding(.top, 40)
+                            .padding(.horizontal, 30)
+                            .font(.title3)
+                        //Password SecureField
+                        SecureField("Введите пароль", text: $password)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(12)
+                            .padding(8)
+                            .padding(.horizontal, 30)
+                            .font(.title3)
+                        Spacer()
+                            .frame(height: 40)
+                        //Login button
+                        Button(action: {}) {
+                            Text("Войти")
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color("CustomGreen"))
+                                .cornerRadius(12)
+                                .padding(8)
+                                .padding(.bottom, 40)
+                                .padding(.horizontal, 30)
+                                .font(.title3.bold())
+                                .foregroundColor(.white)
+                        }.disabled(login.isEmpty || password.isEmpty)
+                        
+                    }.background(Color("CustomLightGray"))
+                        .cornerRadius(30)
+                        .padding(15)
+                }
+                
+            }.onReceive(keyboardIsOnPublisher) { iskeyboardOn in
+                withAnimation(Animation.easeInOut(duration: 0.5)) {
+                    shouldShowLogo = !iskeyboardOn }
+            }
+            
+        }.onTapGesture {
+            UIApplication.shared.endEditing()
+            
+        }.frame(maxWidth:.infinity, maxHeight: .infinity)
+            .background(Color("BrandColor").ignoresSafeArea())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreenView()
+    }
+}
